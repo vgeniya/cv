4ec430fe1b4019d44dc9cc1c02445130cbf04a65
vgeniya <gmenka.13@yandex.ru>
2024-05-20 18:38:47 +0300

    cv

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert(' ОИ НАВЫКИ:   DevTools   Postman   Swagger   YouTrack   Charles   SQL   PostgreSQL   Metabase   Oracle   SQLDeveloper   Figma   REST   JSON   XML   Grafana   Kibana')
+	alert(' МОИ НАВЫКИ:   DevTools   Postman   Swagger   YouTrack   Charles   SQL   PostgreSQL   Metabase   Oracle   SQLDeveloper   Figma   REST   JSON   XML   Grafana   Kibana')
 })
